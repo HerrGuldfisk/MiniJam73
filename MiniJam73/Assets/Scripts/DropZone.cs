e612d0d2afc3d6263d82823e24cccd47a094c8c5
Theodor <hennings.theodor@gmail.com>
2021-02-06 15:25:02 +0100

    stuff

--- MiniJam73/Assets/Scripts/DropZone.cs
+++ MiniJam73/Assets/Scripts/DropZone.cs
@@ -5,12 +5,25 @@ using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler
 {
+	public bool positive;
+
 	public void OnDrop(PointerEventData eventData)
 	{
 		Debug.Log("OnDrop");
 		if ( eventData.pointerDrag != null)
 		{
 			eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+			if (positive)
+			{
+				// Add what happens whe the card is dropped.
+
+				// eventData.pointerDrag...
+			}
+			else
+			{
+				// If a task is denied...
+			}
 		}
 	}
 }
---
+++ MiniJam73/Assets/Scripts/Texts/Texts.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texts : ScriptableObject
+{
+	public Dictionary<string, int> texts;
+
+	public List<string> names;
+
+	private void Awake()
+	{
+		texts = new Dictionary<string, int>();
+		names =
+	}
+}
