c583334c5233128d65ccda58ecd8bafc57c7a173
Theodor <hennings.theodor@gmail.com>
2021-02-06 16:52:16 +0100

    stuff

--- MiniJam73/Assets/Scripts/Texts/Texts.cs
+++ MiniJam73/Assets/Scripts/Texts/Texts.cs
@@ -8,8 +8,10 @@ public class Texts : ScriptableObject
 	public static List<textAndPoints> texts = new List<textAndPoints>
 	{
 		new textAndPoints("The Gods are angry and crave lives to be sacrificed,\n" +
-			"but the voters are stingy and refuses to take one for the team!\n" +
-			"'Sacrifice the Cats! They have 8 lives to spare anyway'", 3),
+			"but the voters are stingy and refuse to take one for the team!\n" +
+			"'Sacrifice the Cats! They have 8 lives to spare anyway'", 5),
+
+
 
 	};
 
