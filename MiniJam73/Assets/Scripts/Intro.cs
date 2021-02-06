b30e8186577993c4a97c6d6e0994f67c1ed0dc46
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2021-02-06 20:28:37 +0100

    Prolog

---
+++ MiniJam73/Assets/Scripts/Intro.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField] Text text;
+    [SerializeField] List<string> texts;
+    [SerializeField] GameObject endButton;
+    [SerializeField] string nextScenePath;
+    int current;
+    int total;
+
+    private void Start()
+    {
+        total = texts.Count;
+        current = 0;
+        text.text = texts[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            current++;
+            if (current < total)
+            {
+                text.text = texts[current];
+            }
+            else
+            {
+                text.text = "";
+                endButton.SetActive(true);
+            }
+        }
+    }
+
+    public void CloseIntro()
+    {
+        SceneManager.LoadScene(nextScenePath);
+    }
+}
