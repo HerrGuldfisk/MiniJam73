6af67d2bfa8b022470cca063471eeefb73a497e4
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2021-02-07 17:37:55 +0100

    playtime hotfix

--- MiniJam73/Assets/Playtime.cs
+++ MiniJam73/Assets/Playtime.cs
@@ -6,22 +6,19 @@ public class Playtime : MonoBehaviour
 {
     public static float playtime = 0;
     public static bool running = false;
-    [SerializeField] bool resetOnAwake;
 
     private void Start()
     {
-        if (resetOnAwake)
-        {
-            playtime = 0;
-            running = true;
-        }
+        playtime = 0;
+        running = true;
     }
 
-    static private void Update()
+    private void Update()
     {
         if (running)
         {
             playtime += Time.deltaTime;
+            Debug.Log(playtime);
         }
     }
 }
